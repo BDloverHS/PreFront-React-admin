e1eb2e8927308df98628b24a856984276cf44a33
BDloverHS <gustn020702naver.com@gmail.com>
2025-02-11 20:42:05 +0900

    Update styledType.ts

--- src/app/global/types/styledType.ts
+++ src/app/global/types/styledType.ts
@@ -2,17 +2,10 @@ export type CommonType = {
   children?: React.ReactNode | string[] | string
   width?: number | string
   height?: number | string
-  color?: string | undefined
+  color?: string | string
   min?: number
   max?: number
   type?: string | undefined
   disabled?: boolean | undefined
   placeholder?: string
-  name?: string
-  value?: string
-  className?: string
-}
-
-export type SelectType = CommonType & {
-  options?: { value: string; label: string }[]
 }
